a90cff6f115cbdc0628520ca7cb08f6413e781d7
mukkaramwaheed <mukkaramwaheed@gmail.com>
2022-06-15 21:20:55 +0500

    Update the message

--- src/components/Constant/index.js
+++ src/components/Constant/index.js
@@ -1,2 +1,2 @@
 export const ERROR_MEG = 'Invalid csv or there is not data in the uploaded csv';
-export const SUCCESS_MEG = 'Your target number match with'
+export const SUCCESS_MEG = 'Parse the CSV and value match with your target number'
