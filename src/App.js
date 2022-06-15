7d3fe147de87a57f28eaf79c31ecf67ecfaaf9b3
mukkaramwaheed <mukkaramwaheed@gmail.com>
2022-06-15 21:21:14 +0500

    Add handler for input field and set state

--- src/App.js
+++ src/App.js
@@ -1,34 +1,44 @@
-import { useState } from 'react'
-import Title from './components/Title'
-import Message from './components/Message';
-import styles from './App.module.css'
-import Papa from 'papaparse'
+import React, { useState, Suspense } from 'react';
+import Title from './components/Title';
+import styles from './App.module.css';
+import Papa from 'papaparse';
 import { ERROR_MEG, SUCCESS_MEG } from './components/Constant';
+const Message = React.lazy(() => import('./components/Message'));
 
 function App() {
-
   const [data, setData] = useState({
     meg: '',
-    error: false
+    error: false,
+  });
+  const [target, setTarget] = useState('');
 
-  })
-  const number = 532;
-  const changeHandler = (event) => {
+  const handlerTarget = (e) => {
+    const value = e.target.value.replace(/\D/g, '');
+    setTarget(value);
+  };
+
+  const handlerCsv = (event) => {
+
+    //Reset meg state
+    setData((data) => ({
+      ...data,
+      meg: '',
+      error: false,
+    }));
+
+    //Parse csv data
     Papa.parse(event?.target?.files[0], {
       header: true,
       skipEmptyLines: false,
       complete: function (results) {
-        let result = results?.meta?.fields?.filter((val) => {
-          let value = parseInt(val)
-          if (!isNaN(val) && val > 0) {
-            return parseInt(value)
-          }
-        })
-        if (result?.length > 0 && result !== undefined) {
-          let getLargestNumber = Math.max(...result)
-          let targetValue = number - getLargestNumber;
-          targetValue += getLargestNumber
-          setData((data) => ({ ...data, meg: `${SUCCESS_MEG} ${targetValue}`, error: false }));
+        let value = getValueFromCsvData(results?.meta?.fields);
+        if (value && value !== undefined && value !== '') {
+          let record = calculateResult(target, value);
+          setData((data) => ({
+            ...data,
+            meg: `${SUCCESS_MEG} ${record}`,
+            error: false,
+          }));
         } else {
           setData((data) => ({
             ...data,
@@ -37,25 +47,78 @@ function App() {
           }));
         }
       },
-    })
-  }
+    });
+  };
+
+  /**
+   * Add and subtract the number from target number
+   * @returns number
+   */
+
+  const calculateResult = (targetNum, value) => {
+    let record = 0;
+    if (value < 0) {
+      let useCsvValue = parseInt(targetNum) + parseInt(value);
+      record = parseInt(useCsvValue) + parseInt(Math.abs(value));
+    } else {
+      let useCsvValue = parseInt(targetNum) + parseInt(value);
+      record = parseInt(useCsvValue) - parseInt(value);
+    }
+    return record;
+  };
+
+  /**
+   * Select single value from csv record
+   * @returns number
+   */
+  const getValueFromCsvData = (arr) => {
+    let value = '';
+    for (let val of arr) {
+      if (Number(val) !== NaN && !isNaN(val)) {
+        value = val;
+        break;
+      }
+    }
+    return value;
+  };
+
+
   return (
     <>
       <header className={styles.header}>
         <Title text='Upload CSV' isWhiteText={true} />
       </header>
       <div className={`${styles.container}`}>
+        <div className={`${styles.marg10}`}>
+          <Suspense fallback={<div>Loading...</div>}>
+            {data?.meg && <Message data={data} />}
+          </Suspense>
+        </div>
+        <div className={`${styles.marg10}`}>
+          <span className={`${styles.margRight15}`}>
+            Set target
+            <b className={`${styles.danger}`}>*</b>
+          </span>
+          <input
+            type='text'
+            name='target'
+            placeholder='Set target value'
+            autoComplete='off'
+            onChange={handlerTarget}
+            value={target}
+          />
+        </div>
         <input
+          disabled={target ? false : true}
           type='file'
           name='file'
-          onChange={changeHandler}
+          onChange={handlerCsv}
+          onClick={(e) => (e.target.value = null)}
           accept='.csv'
-          style={{ display: 'block', margin: '10px auto' }}
         />
-      <Message data={data} />
       </div>
     </>
   );
 }
 
-export default App
+export default App;
