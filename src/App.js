480818a63f80624962cb8687efd20bdc736b273a
mukkaramwaheed <mukkaramwaheed@gmail.com>
2022-06-22 21:56:23 +0500

    Remove set data state

--- src/App.js
+++ src/App.js
@@ -1,54 +1,54 @@
-import React, { useState, Suspense, useEffect } from 'react';
-import Title from './components/Title';
+import React, { useState, useEffect } from 'react';
 import styles from './App.module.css';
+import Title from './components/Title';
 import Papa from 'papaparse';
 import { getEquation } from './components/utilis';
 
 function App() {
   const [data, setData] = useState({
     formula: '',
-    result: ''
+    result: '',
+    sumValues: [],
+    values: [],
   });
-  const [target, setTarget] = useState();
 
-  const calResult = getEquation(
-    [...new Set([0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12])],
-    [4, 13, 8, 12]
-  );
-  console.log('change 1', calResult);
+  const [target, setTarget] = useState(532);
   const handlerTarget = (e) => {
     const value = e.target.value.replace(/\D/g, '');
     setTarget(value);
   };
 
   const handlerCsv = (event) => {
-    //Reset meg state
-    setData((data) => ({
-      ...data,
-      meg: '',
-      error: false,
-    }));
-
     //Parse csv data
     Papa.parse(event?.target?.files[0], {
       header: true,
       skipEmptyLines: false,
       complete: function (results) {
-          const data = getEquation(
-            [...new Set(results)],
-            [4, 13, 8, 12]
-          );
-        console.log('change ', data);
-
+        setData((data) => ({
+          ...data,
+          values: results?.meta?.fields,
+        }));
       },
     });
   };
 
+  const info = (data) => {
+    if (data && data.length > 0) {
+      let copyData = [...data];
+      let result = getEquation([...new Set(copyData)], [4, 13, 16]);
+      return result;
+    }
+  };
+
+  useEffect(() => {
+    info(data.values);
+  }, [data.values]);
+
   return (
     <>
-      <header className={styles.header}>
+      {/* <header className={styles.header}>
         <Title text='Upload CSV ' isWhiteText={true} />
-      </header>
+      </header> */}
       <div className={`${styles.container} ${styles.textCenter}`}>
         <div className={`${styles.marg10}`}>
           <span className={`${styles.margRight15}`}>
