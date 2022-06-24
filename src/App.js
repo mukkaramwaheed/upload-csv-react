50144bbc79ff4630af1c5336a818f2bed89d7242
mukkaramwaheed <mukkaramwaheed@gmail.com>
2022-06-25 00:43:29 +0500

    Update csv parser

--- src/App.js
+++ src/App.js
@@ -1,9 +1,14 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState, useEffect, Suspense } from 'react';
 import styles from './App.module.css';
 import Title from './components/Title';
 import Papa from 'papaparse';
-import { calculateArr, createEquation, createUniqueAndValidArray, getEquation } from './components/utilis';
-
+import {
+  checkValuesExistInCsv,
+  createEquation,
+  createUniqueAndValidArray,
+  showMeg,
+} from './components/utilis';
+const Message = React.lazy(() => import('./components/Message'));
 function App() {
   const [data, setData] = useState({
     formula: '',
@@ -11,56 +16,116 @@ function App() {
     meg: '',
     sumValues: [],
     values: [],
+    formulaValue: '',
   });
-  const targetArr = []
-  const [target, setTarget] = useState(532);
+  const targetArr = [3, 4, 8, 7, 12, 5];
+  const [target, setTarget] = useState('');
   const handlerTarget = (e) => {
     const value = e.target.value.replace(/\D/g, '');
-    setTarget(value);
+    if (value) {
+      setTarget(parseFloat(value));
+    } else {
+      setTarget('');
+    }
   };
 
   const handlerCsv = (event) => {
+    resetState();
     //Parse csv data
     Papa.parse(event?.target?.files[0], {
       header: true,
       skipEmptyLines: false,
       complete: function (results) {
+        let csvValues = results?.meta?.fields;
         setData((data) => ({
           ...data,
-          values: results?.meta?.fields,
+          values: csvValues,
         }));
+        parseCsvValues(csvValues);
       },
     });
   };
 
-  const info = (data) => {
-    if (data && data.length > 0) {
-      let result = createUniqueAndValidArray(data);
+  const parseCsvValues = (parseValues) => {
+    if (parseValues && parseValues?.length > 0) {
+      let result = createUniqueAndValidArray(parseValues);
       if (result && result?.length > 0) {
-        let status = result.find(val => parseFloat(val) === parseFloat(target))
+        let status = result?.find(
+          (val) => parseFloat(val) === parseFloat(target)
+        );
         if (status) {
-          setData(data => ({
+          setData((data) => ({
             ...data,
-            result: true,
-            meg: 'Target value found in the existing csv result'
-          })) 
+            result: false,
+            meg: showMeg('success', 'Target value found in csv'),
+          }));
+        } else if (result && !status) {
+          let valuesExist = checkValuesExistInCsv(targetArr, result);
+          if (valuesExist) {
+            let getData = createEquation(result);
+            if (getData?.value === target) {
+              setData((data) => ({
+                ...data,
+                result: true,
+                formula: getData?.equation,
+                formulaValue: getData?.value,
+                meg: showMeg('success', 'Value matched with the target value'),
+              }));
+            } else {
+              setData((data) => ({
+                ...data,
+                result: false,
+                meg: showMeg('error', 'Target value not created'),
+              }));
+            }
+          } else {
+            setData((data) => ({
+              ...data,
+              result: false,
+              meg: showMeg(
+                'error',
+                'Values that need to create target number not found in csv'
+              ),
+            }));
+          }
+        } else {
+          setData((data) => ({
+            ...data,
+            result: false,
+            meg: showMeg('error', 'Invalid csv'),
+          }));
         }
-        // else if (checkValuesExistInCsv()) {
-        // }
+      } else {
+        setData((data) => ({
+          ...data,
+          result: false,
+          meg: showMeg('error', 'Invalid csv or no number found'),
+        }));
       }
-      return result;
     }
   };
 
+  const resetState = () => {
+    setData((data) => ({
+      ...data,
+      formula: '',
+      result: '',
+      meg: '',
+      sumValues: [],
+      values: [],
+      formulaValue: '',
+    }));
+  };
   useEffect(() => {
-    let copyCsvValues = [...data.values];
-    info(copyCsvValues);
-  }, [data.values]);
+    if (target === '') {
+      resetState();
+    }
+  }, [target]);
 
   return (
     <>
       <header className={styles.header}>
-        <Title text='Upload CSV ' isWhiteText={true} />
+        <Title text='React CSV parser' isWhiteText={true} />
       </header>
       <div className={`${styles.container} ${styles.textCenter}`}>
         <div className={`${styles.marg10}`}>
@@ -85,7 +150,9 @@ function App() {
           onClick={(e) => (e.target.value = null)}
           accept='.csv'
         />
-        <div className={`${styles.marg10}`}>{data.result && data.meg}</div>
+        <Suspense fallback={<div>Loading...</div>}>
+          <Message data={data} styling={styles} />
+        </Suspense>
       </div>
     </>
   );
